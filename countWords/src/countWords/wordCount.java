771b57416967054cab1e5efd4942e9c8bd68776c
mrpav <mrpav@10.0.0.196>
2017-11-18 17:41:52 -0600

    count Words project commit

---
+++ countWords/src/countWords/wordCount.java
@@ -0,0 +1,19 @@
+package countWords;
+
+import java.util.Scanner;
+
+public class wordCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter a String to count the number of words!!");
+		String str=input.nextLine();
+	
+		String[] strArr=str.split(" ");
+		
+		System.out.println("The number of words in a given String is "+strArr.length);
+	}
+
+}
